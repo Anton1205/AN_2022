bff68de464e5fab38eb9feb46309e29771fff6d3
Anton <antonchik18@icloud.com>
2022-11-14 22:43:36 +0300

    files

--- website/main/views.py
+++ website/main/views.py
@@ -4,15 +4,15 @@ from .models import Task
 
 
 def index(request):
-    task = Task.objects.all()
-    return render(request, 'main/index.html', {'title': 'Главная страница', 'tasks': 'title'})
+    task = Task.objects.order_by('id')
+    return render(request, 'main/index.html', {'title': 'Главная страница', 'tasks': task})
 
 
 def about(request):
     task = Task.objects.all()
-    return render(request, 'main/about.html', {'title': 'Описание', 'tasks': 'task'})
+    return render(request, 'main/about.html', {'title': 'Описание', 'tasks': task})
 
 
 def adds(request):
     task = Task.objects.all()
-    return render(request, 'main/adds.html', {'title': 'Дополнительно', 'tasks': 'adds'})
+    return render(request, 'main/adds.html')
